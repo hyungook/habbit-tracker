105b731600d4dcb60ff375d5d8e93e6d9ad6a62e
오형욱 <nenonenoberneno@gmail.com>
2020-12-17 23:02:17 +0900

    React toy-project -ing

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,60 @@
+import React, { Component } from 'react';
 import './app.css';
 import Habits from './components/habits';
+import Navbar from './components/navbar';
 
-function App() {
-  return (
-    <Habits />
-  );
+class App extends Component {
+
+  state = {
+    habits: [
+        { id : 1, name: 'Reading', count: 0 },
+        { id : 2, name: 'Running', count: 0 },
+        { id : 3, name: 'Coding', count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    // console.log(`handleIncrement ${habit.name}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    // this.setState({ habits });
+    this.setState({ habits: habits });
+  }
+  handleDecrement = (habit) => {
+      // console.log(`handleDecrement ${habit}`);
+      const habits = [...this.state.habits];
+      const index = habits.indexOf(habit);
+      const count = habits[index].count - 1;
+      habits[index].count = count < 0 ? 0 : count;  // 좋지 않은 코드
+      this.setState({ habits });
+  }
+
+  handleDelete = (habit) => {
+      // console.log(`handleDelete ${habit}`);
+      const habits = this.state.habits.filter(item => item.id !== habit.id);
+      this.setState({ habits });
+  }
+
+  handleAdd = name => {
+    const habits = [...this.state.habits, {id: Date.now(), name: name, count: 0}];
+    this.setState({habits});
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar totalCount={this.state.habits.filter(item => item.count > 0).length} />
+        <Habits 
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class HabitAddForm extends Component {
+    // 멤버변수를 정의한 후에 원하는 해당하는 대상에 연결하면 된다.
+    formRef = React.createRef();
+    inputRef = React.createRef();
+
+    onSubmit = (event) => {
+        // 브라우저의 기본 기능을 취소
+        event.preventDefault();
+        // console.log(this.inputRef.current.value)
+        const name = this.inputRef.current.value;
+        name && this.props.onAdd(name);
+        // this.inputRef.current.value = '';
+        this.formRef.current.reset();
+    }
+    render() {
+        
+        return (
+            <form ref={this.formRef} className={"add-form"} onSubmit={this.onSubmit}>
+                <input ref={this.inputRef} type={"text"} className={"add-input"} placeholder={"Habit"} />
+                <button className="add-button">Add</button>
+            </form>
+        );
+    }
+}
+
+export default HabitAddForm;
