70a5b093bc56ab1a8a8a93360ade79e2125a1e20
오형욱 <nenonenoberneno@gmail.com>
2020-12-18 22:54:12 +0900

    reset button add

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -33,6 +33,7 @@ class Habits extends Component {
                         />
                         ))}
                 </ul>
+                <button className="habits-reset" onClick={this.props.onReset}>Reset All</button>
             </>
         );
     }
