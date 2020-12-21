b4b74040e9edfde43e4ec45841b82e6cec5c06dc
오형욱 <nenonenoberneno@gmail.com>
2020-12-21 21:22:41 +0900

    function 컴포넌트와 memo

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,28 +1,23 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 
-class HabitAddForm extends PureComponent {
-    // 멤버변수를 정의한 후에 원하는 해당하는 대상에 연결하면 된다.
-    formRef = React.createRef();
-    inputRef = React.createRef();
+const HabitAddForm = memo((props) => {
+    const formRef = React.createRef();
+    const inputRef = React.createRef();
 
-    onSubmit = (event) => {
-        // 브라우저의 기본 기능을 취소
+    const onSubmit = (event) => {
         event.preventDefault();
-        // console.log(this.inputRef.current.value)
-        const name = this.inputRef.current.value;
-        name && this.props.onAdd(name);
-        // this.inputRef.current.value = '';
-        this.formRef.current.reset();
+        const name = inputRef.current.value;
+        name && props.onAdd(name);
+        formRef.current.reset();
     }
-    render() {
-        
-        return (
-            <form ref={this.formRef} className={"add-form"} onSubmit={this.onSubmit}>
-                <input ref={this.inputRef} type={"text"} className={"add-input"} placeholder={"Habit"} />
-                <button className="add-button">Add</button>
-            </form>
-        );
-    }
-}
+
+    return (
+        <form ref={formRef} className={"add-form"} onSubmit={onSubmit}>
+            <input ref={inputRef} type={"text"} className={"add-input"} placeholder={"Habit"} />
+            <button className="add-button">Add</button>
+        </form>
+    );
+});
 
 export default HabitAddForm;
+
